1432af35c6b51bfbbb5a103ff50cb1d1f02084c0
rodartes <rodartes@oregonstate.edu>
2022-05-29 17:51:32 -0700

    fixed errors in reading text files

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ def stripPun(word):
     if "." in word:
         word = word.replace(".", "")
     # look for exclamation
-    elif "!" in word:
+    if "!" in word:
         word = word.replace("!", "")
     # look for question mark
-    elif "?" in word:
+    if "?" in word:
         word = word.replace("?", "")
     # look for comma
-    elif "," in word:
+    if "," in word:
         word = word.replace(",", "")
     # look for apostrophe
-    elif "'" in word:
+    if "'" in word:
         word = word.replace("'", "")
-    elif "(" in word:
+    if "(" in word:
         word = word.replace("(", "")
-    elif ")" in word:
+    if ")" in word:
         word = word.replace(")", "")
     return word.lower()
 
@@ -35,19 +35,18 @@ if len(sys.argv) != 3:
 # open training set
 train = open(sys.argv[1], "r")
 # read data
-train_data = []
-for line in train:
-    train_data.append(train.readline())
+train_data = train.readlines()
 #for item in train_data:
   #print(item)
+#print(train_data)
 train.close()
 
 # open test set
 test = open(sys.argv[2], "r")
 # read data
-test_data = []
-for line in test:
-    test_data.append(test.readline())
+test_data = test.readlines()
+#for line in test:
+#    test_data.append(test.readline())
 #for item in test_data:
     #print(item)
 test.close()
@@ -60,22 +59,30 @@ for item in train_data:
     # seperate each sentence into words
     if "\n" in item:
         item = item.replace("\n", " ")
-    if "0" in item:
-        item = item.replace("0", " ")
-    if "1" in item:
-        item = item.replace("1", " ")
+    #if "0" in item:
+     #   item = item.replace("0", " ")
+    #if "1" in item:
+    #    item = item.replace("1", " ")
     if "\t" in item:
         item = item.replace("\t", " ")
+    if "." in item:
+        item = item.replace(".", " ")
+    if "," in item:
+        item = item.replace(",", " ")
     sentence = item.split(" ")
     # remove any empty strings in sentence
     sentence = list(filter(None, sentence))
     train_sent.append(sentence)
 
+#for i in train_sent:
+    #print(i)
+
 # 2. form the vocabulary (set of words in training data)
 vocab = []
 for s in train_sent:
     for word in s:
-        vocab.append(stripPun(word))
+        if word != '0' and word != '1':
+            vocab.append(stripPun(word))
 print(vocab)
 # 3. convert training and test data
 
